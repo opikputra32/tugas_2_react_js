a40fed06980ee69ebc64fba3dae1990d786c7794
User-niomic <user@niomic.co.id>
2021-08-21 19:40:17 +0700

    Tugas 2 React JS
    Mengerjakan Tugas 2 React JS

---
+++ src/Page/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                <strong>Tugas 2 React Js</strong>
+            </div>
+        );
+    }
+}
+ 
+export default Footer;
